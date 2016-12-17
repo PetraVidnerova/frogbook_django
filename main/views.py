444947d6bf4058ea917a639644ec62264f892e42
Petra Vidnerova <petra.vidnerova@gmail.com>
2016-12-17 19:36:09 +0100

    use bootstrap library carousel on main page

--- main/views.py
+++ main/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     # reset session for debugging
-    request.session['voted'] = [];
+    # request.session['voted'] = [];
     context = {}
     return render(request, "main/index.html", context)
